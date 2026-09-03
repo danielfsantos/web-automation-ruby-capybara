f788fe265d1d70007fd84a0f125cb5b97e9daf5a
Daniel <danielfsantos56@gmail.com>
2026-09-03 13:47:09 -0300

    atualizacao para executar via github actions

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,8 +10,19 @@ RSpec.configure do |config|
 
 Capybara.register_driver :selenium do |app|
 	options = Selenium::WebDriver::Options.chrome
-	options.add_argument('--start-maximized')
-	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+	if ENV['HEADLESS']
+    # Configuração para GitHub Actions / Servidores CI (Linux sem GUI)
+    options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+  else
+    # Configuração para desenvolvimento local
+    options.add_argument('--start-maximized')
+  end
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.default_driver = :selenium
